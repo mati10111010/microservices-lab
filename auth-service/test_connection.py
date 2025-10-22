1d0dce94e84987ca168438639ef614ffae566115
adnvcunrvujnrvmrvro <991573068gem@gmail.com>
2025-10-22 13:13:55 -0500

    ningun cambio

--- auth-service/test_connection.py
+++ auth-service/test_connection.py
@@ -1,11 +1,17 @@
 import os
 import sys
 
-POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'HOST_NO_ENCONTRADO')
-REDIS_HOST = os.environ.get('REDIS_HOST', 'HOST_NO_ENCONTRADO')
+POSTGRES_HOST = os.environ.get(
+    'POSTGRES_HOST', 'HOST_NO_ENCONTRADO'
+    )
+REDIS_HOST = os.environ.get(
+    'REDIS_HOST', 'HOST_NO_ENCONTRADO'
+    )
 
 if POSTGRES_HOST == 'HOST_NO_ENCONTRADO':
-    print("ERROR: POSTGRES_HOST no fue inyectada.")
+    print(
+    "ERROR: POSTGRES_HOST no fue inyectada."
+    )
     sys.exit(1)
 
 print(f"POSTGRES_HOST: {POSTGRES_HOST}")
